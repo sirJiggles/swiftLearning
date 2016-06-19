b8a9b83209bc53aac5b237359090a993f8d2f5a4
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2016-06-19 16:02:39 +0200

    start as 0 on results

--- miles converter/miles converter/ViewController.swift
+++ miles converter/miles converter/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    resultsLabel.text = "0"
     // Do any additional setup after loading the view, typically from a nib.
   }
 
@@ -19,6 +20,7 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
   
   //  Connect the UI elements
   @IBOutlet weak var milesField: UITextField!
