92cd482228bb414df63868ecbacd677fe930da69
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2016-06-19 21:33:16 +0200

    App for messing arround through new boston you tube videos

---
+++ newBoston/newBoston/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newBoston
+//
+//  Created by Gareth Fuller on 19/06/16.
+//  Copyright © 2016 Gareth Fuller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
